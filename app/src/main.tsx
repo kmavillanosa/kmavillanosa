5a93c22a619357fc36850fbd9ef554bcc16a6d2c
Kim Cyriel S. Avillanosa <km.avillanosa@cabinetsbycomputer.com>
2025-12-27 00:44:15 +0800

    general style fix

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+// Ensure light mode is set
+if (typeof document !== 'undefined') {
+	document.documentElement.classList.remove('dark')
+	document.documentElement.classList.add('light')
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<BrowserRouter basename="/kmavillanosa">
